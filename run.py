f55e261125b4faa1685ede1df4f210dceda8c5cc
Mykola Kuievda <dankraig23@gmail.com>
2024-01-11 20:24:43 +0000

    Wrap getting worksheet objects, values and week position in one class

--- run.py
+++ run.py
@@ -44,27 +44,42 @@ WORKSHEET_TITLES = [
 WEEKS_TO_FORECAST = 8
 
 
-print(f'Welcome to the Forward Stock Plan Automation')
+class Worksheets:
+    def __init__(self, worksheet_to_get):
+        self.worksheet_to_get = worksheet_to_get
+
+    def get_values(self):
+        self.retrieved_values = SHEET.worksheet(self.worksheet_to_get).get_all_values()
+        return self.retrieved_values
+    
+    def get_week_index(self, week_number):
+        first_row = self.retrieved_values[0]
+        current_week_index = first_row.index(str(week_number))
+        return current_week_index
+    
+    def get_gspread_worksheet(self):
+        return SHEET.worksheet(self.worksheet_to_get)   
+
 
 def r1c1_to_a1(row, col):
+
     cell = gspread.utils.rowcol_to_a1(row, col)
+
     return cell
 
 def calculate_average_sales(product, week_number):
 
-    # print("Calc ave sales pulling data from the Weekly Sales worksheet")
-    sales_values = SHEET.worksheet(WORKSHEET_TITLES[0]).get_all_values()
-    # print("Retrospective data obtained from the Weekly Sales worksheet")
+    sales = Worksheets(WORKSHEET_TITLES[0])
+    sales_values = sales.get_values()
 
-    first_row = sales_values[0]
-    current_week_index = first_row.index(str(week_number))
+    current_week_index = sales.get_week_index(week_number)
 
     product_range_sale = [
         sublist for sublist in sales_values if product in sublist
     ]
-    
+
     retrospective_sale_strings = [
-        sublist[max(0, current_week_index - 4):current_week_index + 1] 
+        sublist[max(0, current_week_index - 4):current_week_index + 1]
         for sublist in product_range_sale
     ]
 
@@ -79,15 +94,17 @@ def calculate_average_sales(product, week_number):
     return current_week_index, average_sales
 
 
-def update_sales_forecast(product, week_number): 
+def update_sales_forecast(product, week_number):
+
     week_index, retrospective_average_sales = calculate_average_sales(
         product, week_number)
 
     sales_forecast = [
         [mean]*WEEKS_TO_FORECAST for mean in retrospective_average_sales
     ]
-    print(f'Sales Forecast Matrix before updating worksheet:\n {sales_forecast}')
-    sales = SHEET.worksheet(WORKSHEET_TITLES[0])
+
+    sales = Worksheets(WORKSHEET_TITLES[0])
+    sales_gspread = sales.get_gspread_worksheet()
 
     start_column = week_index + 2
     if product == PRODUCT_RANGE[0]:
@@ -95,23 +112,44 @@ def update_sales_forecast(product, week_number):
     elif product == PRODUCT_RANGE[1]:
         start_row = len(PLANTERS) + PLANTERS_START_ROW + 1
     else:
-        print('Product range input error or the Product range does not exist') 
+        print('Product range input error or the Product range does not exist')
 
     start_cell_position = r1c1_to_a1(start_row, start_column)
-    end_row = start_row + len(sales_forecast) 
+    end_row = start_row + len(sales_forecast)
     end_column = start_column + len(sales_forecast[0])
     end_cell_position = r1c1_to_a1(end_row, end_column)
     cells_range = start_cell_position + ':' + end_cell_position
 
     print(f'Start and end cell positions converted: {cells_range}')
 
-    sales.update(cells_range, sales_forecast)
+    sales_gspread.update(cells_range, sales_forecast)
 
     print(f'Sales forecast updated for {WEEKS_TO_FORECAST} weeks')
 
 
+
+def update_forward_stocks(product, week_number):
+
+    sales_values = SHEET.worksheet(WORKSHEET_TITLES[0]).get_all_values()
+    deliveries_values = SHEET.worksheet(WORKSHEET_TITLES[2]).get_all_values()
+    stocks = SHEET.worksheet(WORKSHEET_TITLES[1])
+    stocks_values = stocks.get_all_values()
+
+    first_row = sales_values[0]
+    current_week_index = first_row.index(str(week_number))
+
+
+
+
+
+
 week_of_year = 4
 
-update_sales_forecast(PRODUCT_RANGE[0], week_of_year)
+print(f'Welcome to the Forward Stock Plan Automation')
 
+update_sales_forecast(PRODUCT_RANGE[1], week_of_year)
+
+# week, sales_data = find_current_week_column(week_of_year)
+# print(f'Index of the week in the unpacked sales worksheet: {week}')
+# print(f'Sales list of lists:\n {sales_data}')
 
